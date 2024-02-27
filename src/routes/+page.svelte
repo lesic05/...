db6bd442d0199b0e4a2f0b974d7b5e750627bdad
lesicl05 <80815302+lesic05@users.noreply.github.com>
2024-02-27 14:36:42 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,13 @@
 	import { supabase } from '$lib/supabase';
 
 	let promise = supabase.from('countries').select();
-	let countries = [];
+	let countries = [supabase.from('countries').select('name')];
 </script>
 
-<h1>Aufgaben:</h1>
-<ul>
-	<li>
-		anstatt eines JS-Objekts, das auf der Seite dargestellt wird, möchte ich eine Liste aller Länder
-	</li>
-	<li>anstatt loading, loading spinner nehmen</li>
-</ul>
 {#await promise}
 	<span class="loading loading-spinner loading-lg" />
 {:then result}
-	<div>{JSON.stringify(result)}</div>
+	{#each result.data as country}
+		<p>{country.name}</p>
+	{/each}
 {/await}
-
-{#each countries as country}
-	<div>
-		<h2>{country.name}</h2>
-	</div>
-{/each}
