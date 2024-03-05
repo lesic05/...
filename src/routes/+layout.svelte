6b6ed70a5957dadf306a873daf45ee2cd87a876e
lesicl05 <80815302+lesic05@users.noreply.github.com>
2024-03-05 14:28:51 +0100

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
 	import '../app.postcss';
-	let theme = 'cyberpunk';
+	import { theme } from '$lib/store';
 </script>
 
-<div data-theme={theme} class="min-h-screen p-3">
+<div data-theme={$theme} class="min-h-screen p-3">
 	<!-- <nav class="flex items-center gap-8 m-4">
 		<a href="/" class="text-center">Home</a>|
 		<a href="/students" class="text-center">Students <br />(Dynamic Routing)</a>|
@@ -13,7 +13,7 @@
 		<a href="/universal-stream" class="text-center">Universal-Stream</a>
 	</nav> -->
 
-	<select bind:value={theme} class="fixed top-0 right-0">
+	<select bind:value={$theme} class="fixed top-0 right-0">
 		<option value="cyberpunk">cyberpunk</option>
 		<option value="coffee">coffee</option>
 		<option value="synthwave">synthwave</option>
